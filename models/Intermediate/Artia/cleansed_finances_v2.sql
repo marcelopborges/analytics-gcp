0aec2ce444e062ce65f027c22048d09cc51e99cb
Marcelo Borges <marcelo.b.org@outlook.com>
2024-02-29 13:31:13 +0000

    refatorando código

---
+++ models/Intermediate/Artia/cleansed_finances_v2.sql
@@ -0,0 +1,54 @@
+{{  
+    config(materialized='view')
+}}
+
+
+with cleansed_finances as
+( 
+    select * from {{ ref('stg_finances_v2') }}
+)
+
+select
+id
+,date
+,kind
+,unit
+,title
+,amount
+,user_id
+,folder_id
+,INITCAP(user_name) as user_name
+,account_id
+,breadcrumb
+,unit_value
+,activity_id
+,cost_center
+,description
+,folder_name
+,object_type
+,total_value
+,account_name
+,finance_date
+,finance_kind
+,finance_unit
+,time_entry_id
+,finance_amount
+,user_role_name
+,created_for_user
+,financial_rating
+,INITCAP(responsible_name) as responsible_name
+,expense_report_id
+,responsible_email
+,finance_unit_value
+,refund_by_customer
+,finance_cost_center
+,finance_total_value
+,responsible_user_id
+,expense_report_status
+,INITCAP(responsible_user_name) as responsible_user_name
+,folder_last_project_id
+,general_ledger_account
+,account_organization_id
+,finance_financial_rating
+,finance_general_ledger_account
+from cleansed_finances
--- models/Staging/Artia/stg_projects_v2.sql
+++ models/Staging/Artia/stg_projects_v2.sql
@@ -1,1 +1,11 @@
-SELECT * FROM `mvp-hp.dados_hp_airbyte.organization_76632_projects_v2`
+{{ config(materialized='view') }}
+
+
+with stg_projects_v2 as 
+(
+    select * 
+    from `mvp-hp.dados_hp_airbyte.organization_76632_projects_v2`
+)
+
+select * 
+from stg_projects_v2
